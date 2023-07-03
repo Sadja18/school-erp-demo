8ef84edd3f39ccc77dc4c8c1b5578fadcda40c3c
Naman Mishra <namanmishraec1045@gmail.com>
2023-07-03 17:36:58 +0530

    renaming

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -29,7 +29,7 @@ class UsersTableSeeder extends Seeder
 
         $d = [
 
-            ['name' => 'CJ Inspired',
+            ['name' => 'Sadja Learning',
                 'email' => 'cj@cj.com',
                 'username' => 'cj',
                 'password' => $password,
